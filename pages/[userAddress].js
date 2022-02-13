365b302cfb398469bcb3322c3c752e9a00c4975a
malikhadjri <mjhadjri@gmail.com>
2022-02-13 16:45:24 -0500

    Set up user profiles page with GH connect

---
+++ pages/[userAddress].js
@@ -0,0 +1,125 @@
+import { useRouter } from "next/router";
+import {hasEthereum} from "../utils/ethereum";
+import {ethers} from "ethers";
+import {useState, useRef, useEffect} from "react";
+import Fuse from '../src/artifacts/contracts/Fuse.sol/Fuse.json'
+import {GithubProvider} from "../utils/githubProvider.tsx";
+
+export default function PostPage() {
+
+    const [connectedAddressGithub, setConnectedAddressGithub] = useState('')
+    const [newGithubUsername, setNewGithubUsernameState] = useState('')
+    const [currentProfile, setCurrentProfile] = useState('')
+    const [newInfoMessage, setInfoMessageState] = useState('')
+    const newGithubUsernameInputRef = useRef();
+    const router = useRouter();
+
+    useEffect( () => {
+    },[connectedAddressGithub])
+
+    async function fetchConnectedWalletProfile() {
+        if ( ! hasEthereum() ) {
+            setConnectedWalletAddressState(`MetaMask unavailable`)
+            return
+        }
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const contract = new ethers.Contract(process.env.NEXT_PUBLIC_FUSE_ADDRESS, Fuse.abi, provider)
+        try {
+            const data = await contract.profiles(router.query.userAddress)
+            console.log(data)
+            setCurrentProfile(data)
+
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
+    // Call smart contract, fetch current value
+    async function fetchConnectedAddressGithub() {
+        if ( ! hasEthereum() ) {
+            setConnectedWalletAddressState(`MetaMask unavailable`)
+            return
+        }
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const contract = new ethers.Contract(process.env.NEXT_PUBLIC_FUSE_ADDRESS, Fuse.abi, provider)
+        try {
+            const data = await contract.getUserGithub(router.query.userAddress)
+            setConnectedAddressGithub(data)
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
+    // Call smart contract, set new value
+    async function setGithubUsername() {
+        if ( ! hasEthereum() ) {
+            setConnectedWalletAddressState(`MetaMask unavailable`)
+            return
+        }
+        if(! newGithubUsername ) {
+            setInfoMessageState('Input something first!')
+            return
+        }
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(process.env.NEXT_PUBLIC_FUSE_ADDRESS, Fuse.abi, signer)
+        const transaction = await contract.setUserProfileGithub(router.query.userAddress, newGithubUsername)
+        await transaction.wait()
+        setInfoMessageState(`Github username updated to ${newGithubUsername} from ${connectedAddressGithub}.`)
+        newGithubUsernameInputRef.current.value = ''
+        setNewGithubUsernameState('')
+    }
+
+    async function getGithub(){
+        //this should later trigger an oauth flow so that user has to connect github rather than hardcoded user profile
+        //githubprovider.getUser
+        //stores to state, then on mint it's saved along with the user's ethereum address to the mainnet
+        GithubProvider.getRepo(connectedAddressGithub).then(
+            (res) => console.log(res)
+        )
+    }
+
+    return (
+        <div className="max-w-lg mt-36 mx-auto text-center px-4">
+            <h1 className="text-4xl font-semibold mb-8">Welcome! #{router.query.userAddress}</h1>
+            <div className="space-y-8">
+                <div className="flex flex-col space-y-4">
+                  <input
+                    className="border p-4 w-100 text-center"
+                    placeholder="A fetched github username will show here"
+                    value={connectedAddressGithub}
+                    disabled
+                  />
+                  <button
+                      className="bg-blue-600 hover:bg-blue-700 text-white py-4 px-8 rounded-md w-full"
+                      onClick={fetchConnectedAddressGithub}
+                    >
+                      Fetch github username from the blockchain
+                    </button>
+                </div>
+                <div className="space-y-8">
+                  <div className="flex flex-col space-y-4">
+                      <input
+                          className="border p-4 text-center"
+                          onChange={ e => setNewGithubUsernameState(e.target.value)}
+                          placeholder="Write a new github username"
+                          ref={newGithubUsernameInputRef}
+                      />
+                      <button
+                          className="bg-blue-600 hover:bg-blue-700 text-white py-4 px-8 rounded-md"
+                          onClick={setGithubUsername}
+                      >
+                          Set new github username on the blockchain
+                      </button>
+                      <button onClick={getGithub} className="bg-blue-600 hover:bg-blue-700 text-white py-4 px-8 rounded-md">
+                          Connect your GitHub
+                      </button>
+                  </div>
+                </div>
+                <div className="h-2">
+                  { newInfoMessage && <span className="text-sm text-gray-500 italic">{newInfoMessage}</span> }
+                </div>
+            </div>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,14 @@
 import Head from 'next/head'
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import { ethers } from 'ethers'
 import { hasEthereum } from '../utils/ethereum'
 import Fuse from '../src/artifacts/contracts/Fuse.sol/Fuse.json'
 
 export default function Home() {
-  const [connectedAddressGithub, setConnectedAddressGithub] = useState('')
-  const [newGithubUsername, setNewGithubUsernameState] = useState('')
-  const [newInfoMessage, setInfoMessageState] = useState('')
   const [connectedWalletAddress, setConnectedWalletAddressState] = useState('')
   const router = useRouter()
   const [profiles, setProfiles] = useState('')
-  const [currentProfile, setCurrentProfile] = useState('')
-  const newGithubUsernameInputRef = useRef();
 
   // If wallet is already connected...
   useEffect( () => {
@@ -27,21 +22,30 @@ export default function Home() {
       try {
         const signerAddress = await signer.getAddress()
         setConnectedWalletAddressState(signerAddress)
-        console.log(signerAddress)
-        console.log("signerAddress")
-        fetchConnectedWalletProfile();
       } catch {
         setConnectedWalletAddressState('No wallet connected')
-
       }
     }
     setConnectedWalletAddress();
     fetchRegisteredProfiles();
-  },[])
+  },[currentProfile])
 
   // Request access to MetaMask account
   async function requestAccount() {
     await window.ethereum.request({ method: 'eth_requestAccounts' } )
+  }
+
+  async function connectWallet() {
+    if ( ! hasEthereum() ) {
+      setConnectedWalletAddressState(`MetaMask unavailable`)
+      return
+    }
+
+    await requestAccount()
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner()
+    const signerAddress = await signer.getAddress()
+    router.push('/' + signerAddress)
   }
 
   async function fetchRegisteredProfiles() {
@@ -58,80 +62,6 @@ export default function Home() {
     } catch(error) {
       console.log(error)
     }
-  }
-
-  async function fetchConnectedWalletProfile() {
-    if ( ! hasEthereum() ) {
-      setConnectedWalletAddressState(`MetaMask unavailable`)
-      return
-    }
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    const contract = new ethers.Contract(process.env.NEXT_PUBLIC_FUSE_ADDRESS, Fuse.abi, provider)
-    try {
-      console.log(connectedWalletAddress)
-      console.log("signerAddress")
-      const data = await contract.profiles(connectedWalletAddress)
-      console.log(data)
-      setCurrentProfile(data)
-    } catch(error) {
-      console.log(error)
-    }
-  }
-
-  // Call smart contract, fetch current value
-  async function fetchConnectedAddressGithub() {
-    if ( ! hasEthereum() ) {
-      setConnectedWalletAddressState(`MetaMask unavailable`)
-      return
-    }
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    const contract = new ethers.Contract(process.env.NEXT_PUBLIC_FUSE_ADDRESS, Fuse.abi, provider)
-    try {
-      console.log("address is:" + connectedWalletAddress)
-      const data = await contract.getUserGithub(connectedWalletAddress)
-      setConnectedAddressGithub(data)
-    } catch(error) {
-      console.log(error)
-    }
-  }
-
-  // Call smart contract, set new value
-  async function setGithubUsername() {
-    if ( ! hasEthereum() ) {
-      setConnectedWalletAddressState(`MetaMask unavailable`)
-      return
-    }
-    if(! newGithubUsername ) {
-      setInfoMessageState('Input something first!')
-      return
-    }
-    await requestAccount()
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner()
-    const signerAddress = await signer.getAddress()
-    setConnectedWalletAddressState(signerAddress)
-    const contract = new ethers.Contract(process.env.NEXT_PUBLIC_FUSE_ADDRESS, Fuse.abi, signer)
-    const transaction = await contract.setUserProfileGithub(signerAddress, newGithubUsername)
-    await transaction.wait()
-    setInfoMessageState(`Github username updated to ${newGithubUsername} from ${connectedAddressGithub}.`)
-    newGithubUsernameInputRef.current.value = ''
-    setNewGithubUsernameState('')
-  }
-
-  async function connectWallet() {
-    if ( ! hasEthereum() ) {
-      setConnectedWalletAddressState(`MetaMask unavailable`)
-      return
-    }
-
-    await requestAccount()
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner()
-    const signerAddress = await signer.getAddress()
-    setConnectedWalletAddressState(`Connected wallet: ${signerAddress}`)
-    router.push('/makeProfile')
-
-
   }
 
   return (
@@ -153,41 +83,40 @@ export default function Home() {
               Fuse Pass
             </h1>
             <p>Profiles: {profiles}</p>
-            <p>Current Profile: {currentProfile}</p>
             <div className="space-y-8">
-                <div className="flex flex-col space-y-4">
-                  <input
-                    className="border p-4 w-100 text-center"
-                    placeholder="A fetched github username will show here"
-                    value={connectedAddressGithub}
-                    disabled
-                  />
-                  <button
-                      className="bg-blue-600 hover:bg-blue-700 text-white py-4 px-8 rounded-md w-full"
-                      onClick={fetchConnectedAddressGithub}
-                    >
-                      Fetch github username from the blockchain
-                    </button>
-                </div>
-                <div className="space-y-8">
-                  <div className="flex flex-col space-y-4">
-                    <input
-                      className="border p-4 text-center"
-                      onChange={ e => setNewGithubUsernameState(e.target.value)}
-                      placeholder="Write a new github username"
-                      ref={newGithubUsernameInputRef}
-                    />
-                    <button
-                      className="bg-blue-600 hover:bg-blue-700 text-white py-4 px-8 rounded-md"
-                      onClick={setGithubUsername}
-                    >
-                      Set new github username on the blockchain
-                    </button>
-                    <div className="h-2">
-                      { newInfoMessage && <span className="text-sm text-gray-500 italic">{newInfoMessage}</span> }
-                    </div>
-                  </div>
-                </div> */}
+                {/*<div className="flex flex-col space-y-4">*/}
+                {/*  <input*/}
+                {/*    className="border p-4 w-100 text-center"*/}
+                {/*    placeholder="A fetched github username will show here"*/}
+                {/*    value={connectedAddressGithub}*/}
+                {/*    disabled*/}
+                {/*  />*/}
+                {/*  <button*/}
+                {/*      className="bg-blue-600 hover:bg-blue-700 text-white py-4 px-8 rounded-md w-full"*/}
+                {/*      onClick={fetchConnectedWalletProfile}*/}
+                {/*    >*/}
+                {/*      Fetch github username from the blockchain*/}
+                {/*    </button>*/}
+                {/*</div>*/}
+                {/*<div className="space-y-8">*/}
+                {/*  <div className="flex flex-col space-y-4">*/}
+                {/*    <input*/}
+                {/*      className="border p-4 text-center"*/}
+                {/*      onChange={ e => setNewGithubUsernameState(e.target.value)}*/}
+                {/*      placeholder="Write a new github username"*/}
+                {/*      ref={newGithubUsernameInputRef}*/}
+                {/*    />*/}
+                {/*    <button*/}
+                {/*      className="bg-blue-600 hover:bg-blue-700 text-white py-4 px-8 rounded-md"*/}
+                {/*      onClick={setGithubUsername}*/}
+                {/*    >*/}
+                {/*      Set new github username on the blockchain*/}
+                {/*    </button>*/}
+                {/*    <div className="h-2">*/}
+                {/*      { newInfoMessage && <span className="text-sm text-gray-500 italic">{newInfoMessage}</span> }*/}
+                {/*    </div>*/}
+                {/*  </div>*/}
+                {/*</div>*/}
                 <div>
                   <button
                   className="bg-blue-600 hover:bg-blue-700 text-white py-4 px-8 rounded-md"
@@ -203,17 +132,6 @@ export default function Home() {
           </>
         ) }
       </main>
-
-      <footer className="mt-20">
-        <a
-          href="https://github.com/tomhirst/solidity-nextjs-starter/blob/main/README.md"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-blue-600 hover:text-blue-700"
-        >
-          Read the docs
-        </a>
-      </footer>
     </div>
   )
 }
